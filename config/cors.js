2842d808d010c6d27d1beab9d4fcabb9267214bb
Johanan Oppong Amoateng <88411614+JohananOppongAmoateng@users.noreply.github.com>
2025-07-06 23:49:03 +0000

    Update cors.js

--- config/cors.js
+++ config/cors.js
@@ -2,7 +2,7 @@ import dotEnv from "dotenv";
 dotEnv.config();
 
 export const corsOptions = {
-    origin: [process.env.DOMAIN, "http://localhost:5173","http://localhost:5174","https://speg-ecommerce-frontend.onrender.com"], // or a specific URL
+    origin: [process.env.DOMAIN, "http://localhost:5173","http://localhost:5174","https://speg-ecommerce-frontend.onrender.com","https://speg-admin-frontend.onrender.com"], // or a specific URL
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true // To allow cookies with CORS
 };
